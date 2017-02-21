76b012d18b1a8384d515b642ffd3685415c06cc0
Alejandro Botas <abotas@gpunextserv.uv.es>
2017-02-21 19:07:44 +0100

    initial functional anastasia!

--- invisible_cities/cities/anastasia.py
+++ invisible_cities/cities/anastasia.py
@@ -34,6 +34,7 @@ class Anastasia(DetectorResponseCity):
                  nprint     = 10000,
                  
                  # Parameters added at this level
+                 NEVENTS = 0,
                  stuff_NEW = False,
                  window_energy_threshold = 0.05,
                  d_cut = 15 # mm
@@ -42,14 +43,27 @@ class Anastasia(DetectorResponseCity):
         DetectorResponseCity.__init__(self,
                                       run_number = run_number,
                                       files_in   = files_in,
-                                      file_out   = None,
+                                      file_out   = file_out,
                                       nprint     = nprint)
         
-        
+        self.NEVENTS = NEVENTS
         self.stuff_NEW = stuff_NEW
         self.window_energy_threshold = window_energy_threshold
         self.d_cut = d_cut
+        
+    def set_output_earray(self):
+        """
+        Define location for appending SiPM maps for each event
+        """
 
+        f_out      = tb.open_file(self.output_file, 'w')
+        filters    = tb.Filters(complib='blosc', complevel=9, shuffle=False)
+        atom       = tb.Atom.from_dtype(np.dtype('Float32'))
+        tmaps      = f_out.create_earray(f_out.root, 'maps', atom, 
+                                     (0, 20, 20, 60), filters=filters) 
+        self.tmaps = tmaps
+        self.f_out = f_out
+        
     def sliding_window(self, E):
         """
         sliding window finds a 20x20x60 window of sipms that have detected
@@ -83,13 +97,13 @@ class Anastasia(DetectorResponseCity):
         zb = np.array([zcenter - 59, zcenter + 59], dtype=np.int16) # us
 
         # Stuff inside 235mm x 235mm
-        if   xb[0] < min_xp: xb = xb + (self.min_xp - xb[0])
-        elif xb[1] > max_xp: xb = xb - (xb[1]  - self.max_xp)
-        if   yb[0] < min_yp: yb = yb + (self.min_yp - yb[0])
-        elif yb[1] > max_yp: yb = yb - (yb[1]  - self.max_yp)
+        if   xb[0] < self.min_xp: xb = xb + (self.min_xp - xb[0])
+        elif xb[1] > self.max_xp: xb = xb - (xb[1]  - self.max_xp)
+        if   yb[0] < self.min_yp: yb = yb + (self.min_yp - yb[0])
+        elif yb[1] > self.max_yp: yb = yb - (yb[1]  - self.max_yp)
 
         # Put inside NEW geometry
-        if stuff_NEW: 
+        if self.stuff_NEW: 
 
             # Eliminate the four 80x20 corners
             if xb[0] < -155:
@@ -114,7 +128,7 @@ class Anastasia(DetectorResponseCity):
         if zb[0] < self.min_zp:
             zb -= zb[0]
         elif zb[1] > self.max_zp / self.drift_speed:
-            zb = zb - (zb[1] - np.ceil(max_zp / self.drift_speed)).astype(np.int32)
+            zb = zb - (zb[1] - np.ceil(self.max_zp / self.drift_speed)).astype(np.int32)
 
         # Get SiPM positions, adding pitch because boarders are inclusive
         # Notice maps will be organized as follows:
@@ -141,10 +155,121 @@ class Anastasia(DetectorResponseCity):
         else: return (E[np.logical_not(out_e)], xpos, ypos, zpos)
         
 
+    def generate_s2(self):
+        """
+        -Loops over all the signal/background events in all the (desired) files 
+
+        -Calls drift_electrons for e ach event to determine drifting electrons' 
+        coordinates when electrons reach the EL.
+
+        -Calls sliding_window, to find 3d 'window' of sipms central SiPMs
+        for each event.
+
+        -Calls EL_smear to compute SiPM_response if applying EL smear. 
+        Otherwise calls SiPM_responses directly to compute SiPM response
+        within the window for each event.
+
+        -Saves data for event by event because pytables EArray is fast, we
+        do not know number of events ahead of time, np.concatenate can be
+        slow, and this way we don't need to hold maps memory.
+
+        arguments: 
+        tmaps is the earray in file where SiPM maps are to be saved
+        """
+
+        accepted_events  = 0
+        discarded_events = 0
+
+        # Loop over each desired file in filespath
+        for fn in self.input_files:
+
+            if accepted_events == self.NEVENTS: break
+
+            #print('Processing file ' + str(fn))
+            f_mc = tb.open_file(fn, 'r')
+            ptab = f_mc.root.MC.MCTracks
+
+            file_explored = False
+            nrow = 0 # need to track current row in pytable since events are not separated
+
+            # Iterates over each event
+            while not file_explored:
+
+                # Call drift_electrons
+                (electrons, nrow, file_explored) = drift_electrons(
+                    ptab, nrow, self.max_energy, self.transverse_diffusion, 
+                    self.longitudinal_diffusion, self.drift_speed, self.w_val, 
+                    self.electrons_prod_F)
+
+                # Find appropriate window
+                try: (electrons, xpos, ypos, zpos) = self.sliding_window(electrons)   
+
+                except ValueError:  
+
+                    # Or discard
+                    if self.sliding_window(electrons) == 'Window Cut':
+                        discarded_events += 1
+                        continue
+
+                    else: raise
+
+                # z in units of time bin
+                TS  = (electrons[:, 2] - zpos[0]) / self.zpitch 
+                if (TS < -1).any():
+                    print(TS[np.where(TS < 0)[0]])
+                    raise ValueError('Electrons are outside z window')    
+
+                # Use EL_smear to get SiPM maps
+                if self.zmear: 
+                    ev_maps = EL_smear(TS, E, xpos, ypos, self.xydim, 
+                                       self.el_width, self.el_sipm_d, 
+                                       self.t_gain, self.gain_nf)
+
+                # Call SiPM_response directly
+                else:
+
+                    # SiPM maps for an event
+                    ev_maps = np.zeros((self.xydim, self.xydim, self.zdim), 
+                                       dtype=np.float32)
+
+                    if self.gain_nf != 0:
+                        G = np.random.normal(self.t_gain, 
+                            scale=np.sqrt(self.t_gain * self.gain_nf), 
+                            size=(len(electrons),))
+                    else:
+                        G = np.ones((len(electrons),), dtype=np.float32) * self.t_gain
+
+                    # Sum SiPM response for each electron
+                    for f_ts, e, g in zip(np.array(np.floor(TS), dtype=np.int8), electrons, G):
+
+                        if f_ts < 0: continue # Only relevant for zmear
+
+                        ev_maps[:, :, f_ts] += SiPM_response(e, xpos, ypos, 
+                            np.array([self.el_sipm_d + self.el_width, self.el_sipm_d], 
+                            dtype=np.float32), g, self.xydim)
+
+                # Add noise in detection probability
+                if self.photon_detection_noise: ev_maps += np.random.poisson(ev_maps)
+
+                # Save the event's maps
+                self.tmaps.append([ev_maps])
+
+                accepted_events += 1
+                if accepted_events == self.NEVENTS: break
+
+            f_mc.close()
+            
+        print(self.f_out)
+        self.f_out.close()
+      
+    
+    
+    
 conf = read_config_file('/home/abotas/IC-1/invisible_cities/config/anastasia.conf')
 
 
 A = Anastasia(
+    NEVENTS  = conf['NEVENTS'],
     files_in = glob(conf['FILE_IN']),
     file_out = conf['FILE_OUT'],
     )
@@ -166,8 +291,9 @@ A.set_sensor_response_params(conf['t_gain'],   conf['gain_nf'],
                              conf['num_bins'], conf['zmear'], 
                              conf['photon_detection_noise'])
 
+A.set_output_earray()
 
-
+A.generate_s2()
 
     
       
