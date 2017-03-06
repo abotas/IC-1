9b679fb4dd0ab28ae9f049eb1c9137348dbae639
Alejandro Botas <alej@Alejandros-MacBook-Pro.local>
2017-03-06 22:04:14 +0100

    tests for HPXeEL and Box classes

--- invisible_cities/cities/anastasia_test.py
+++ invisible_cities/cities/anastasia_test.py
@@ -8,11 +8,17 @@ from invisible_cities.core.detector_response_functions import \
      diffuse_electrons, \
      sliding_window, \
      bin_EL, \
-     SiPM_response
-    
-from invisible_cities.core.configure   import configure
-from invisible_cities.cities.anastasia import Anastasia, ANASTASIA   
-    
+     SiPM_response, \
+     HPXeEL
+
+
+from invisible_cities.core.detector_geometry_functions import Box, \
+     TrackingPlaneBox, TrackingPlaneResponseBox
+
+from invisible_cities.core.configure         import configure
+from invisible_cities.cities.anastasia       import Anastasia, ANASTASIA
+from invisible_cities.core.system_of_units_c import units
+
 config_file_format = """
 # set_input_files
 PATH_IN {PATH_IN}
@@ -48,17 +54,17 @@ max_zp {max_zp}
 el_sipm_d {el_sipm_d}
 el_width {el_width}
 xydim {xydim}
-zdim {zdim}  
+zdim {zdim}
 xypitch {xypitch}
-zpitch {zpitch} 
+zpitch {zpitch}
 
-gain_nf {gain_nf} 
+gain_nf {gain_nf}
 t_gain {t_gain}
 el_traverse_time {el_traverse_time}
 photon_detection_noise {photon_detection_noise}
 zmear {zmear}
 """
-       
+
 def config_file_spec_with_tmpdir(tmpdir):
     return dict(PATH_IN  = '$ICDIR/database/test_data/',
                 FILE_IN  = 'NEW_se_mc_1evt.h5',
@@ -69,14 +75,14 @@ def config_file_spec_with_tmpdir(tmpdir):
                 NEVENTS     =     1,
                 NPRINT      =     0,
                 RUN_ALL     = False,
-               
+
                 electrons_prod_F = False,
                 max_energy       = 2.6e6,
                 reduce_electrons = 100  ,
                 w_val            = 22.4 ,
-               
-                longitudinal_diffusion  = 3  , 
-                transverse_diffusion    = 10 , 
+
+                longitudinal_diffusion  = 3  ,
+                transverse_diffusion    = 10 ,
                 drift_speed             = 1.0,
                 window_energy_threshold = 0.05,
                 d_cut     =  15 ,
@@ -89,11 +95,11 @@ def config_file_spec_with_tmpdir(tmpdir):
                 el_sipm_d =  5  ,
                 el_width  =  5  ,
                 xydim     =  20 ,
-                zdim      =  60 ,  
+                zdim      =  60 ,
                 xypitch   =  10 ,
-                zpitch    =  2.0, 
+                zpitch    =  2.0,
 
-                gain_nf   = 0.0, 
+                gain_nf   = 0.0,
                 t_gain    = 1050.0,
                 el_traverse_time = 2,
                 photon_detection_noise = False,
@@ -106,26 +112,26 @@ def test_command_line_Anastasia(config_tmpdir):
     with open(conf_file_name, 'w') as conf_file:
         conf_file.write(config_file_contents)
     ANASTASIA(['ANASTASIA', '-c', conf_file_name])
-    
+
 def test_sliding_window(config_tmpdir):
     """
     1) check that sliding window is inside bounds perscribed by
     min_xp, min_yp etc
-    
+
     2) check that electrons of distance greater than d_cut
     from the window in x or y are discarded
-    
+
     3) check that electrons that occur more than EL_traverse_time
     from window in z are discarded
-    
+
     4) check to see that sliding window cuts events with
     more than x% of energy outside the window
-    
+
     5) check to see that window creates correct window
-    
+
     kwargs:
     T, is the outside-window-energy-fraction, above which
-    events are cut. 
+    events are cut.
     """
     max_xp = 235; min_xp = -235
     max_yp = 235; min_yp = -235
@@ -139,35 +145,35 @@ def test_sliding_window(config_tmpdir):
 
     E_1     = np.empty((1000, 3), dtype=np.float32)
     E_1[:,] = np.array([max_xp + 1, 0, min_zp])
-    
-    # Exceed x boundaries 
+
+    # Exceed x boundaries
     E_1[0, 0] = max_xp + d_cut + 1
     E_1[2, 0] = min_xp - d_cut - 1
-    
-    # Exceed y boundaries 
+
+    # Exceed y boundaries
     E_1[5, 1] =  d_cut + max_yp + 1
     E_1[6, 1] = -d_cut + min_yp - 1
-    
+
     # Check z boundaries
     E_1[8, 2]  =  el_traverse_time + max_zp / drift_speed + 1
     E_1[-1, 2] = -el_traverse_time - 1
-    
+
     # Check redundancy
     E_1[2, 1] =  d_cut + max_yp + 1
     E_1[8, 0] = -d_cut + min_xp - 1
-    
+
     # (4)
     EPOS = sliding_window(E_1, xydim, zdim, xypitch, zpitch, min_xp, max_xp,
                    min_yp, max_yp, min_zp, max_zp, d_cut, el_traverse_time,
                    drift_speed, window_energy_threshold)
-    
+
     assert EPOS != 'Window Cut'
-    
+
     try:    (E_1_out, xpos, ypos, zpos) = EPOS
     except: ValueError
-    
-    # (1, 2, 3)  
-    assert len(E_1_out) == 994        
+
+    # (1, 2, 3)
+    assert len(E_1_out) == 994
     assert xpos[ 0] == max_xp - (xydim - 1) * xypitch
     assert xpos[-1] == max_xp
     assert ypos[ 0] == -5 - (xydim / 2 - 1) * xypitch
@@ -177,24 +183,24 @@ def test_sliding_window(config_tmpdir):
 
     # (4)
     assert sliding_window(E_1[:100],
-                          xydim, zdim, 
-                          xypitch, zpitch, 
+                          xydim, zdim,
+                          xypitch, zpitch,
                           min_xp, max_xp,
-                          min_yp, max_yp, 
-                          min_zp, max_zp, 
-                          d_cut, 
+                          min_yp, max_yp,
+                          min_zp, max_zp,
+                          d_cut,
                           el_traverse_time,
-                          drift_speed, 
+                          drift_speed,
                           window_energy_threshold) == 'Window Cut'
-    
+
 def test_SiPM_response():
     """
     Check that SiPM_response returns correct map for a couple
     electrons
-    
-    ** MUST UPDATE IF P(detection) CHANGED! ** 
+
+    ** MUST UPDATE IF P(detection) CHANGED! **
     """
-    
+
     # Sample electron/window
     E = np.array([[3, 41, 0]])
     xpos = np.array(range(5))*10 + 5
@@ -204,28 +210,28 @@ def test_SiPM_response():
     el_sipm_d = 5
     el_width  = 5
     m = np.zeros((len(xpos), len(ypos)), dtype=np.float32)
-    
+
     # Get map
     for e in E:
-        m += SiPM_response(e, xpos, ypos, xydim, 
-                           [el_width + el_sipm_d, el_sipm_d], 
+        m += SiPM_response(e, xpos, ypos, xydim,
+                           [el_width + el_sipm_d, el_sipm_d],
                            gain)
-    
+
     # Check equality
     for row, x in zip(m, xpos):
         for resp, y in zip(row, ypos):
-            
+
             check = gain * np.array([1.0 / np.sqrt(
                 (E[0, 0] - x)**2 + (E[0, 1] - y)**2 + el_sipm_d**2) - \
                 1  / np.sqrt((
                 E[0, 0] - x)**2 + (E[0, 1] - y)**2 + (el_sipm_d + el_width)**2)],
                 dtype=np.float32) / float(4) / float(el_width)
-            
+
             # np.isclose because these are floats
             assert np.isclose(resp, check[0], rtol=1e-8, atol=1e-9)
-            
-            
-            
+
+
+
 def test_generate_ionization_electrons():
 
     tf = tb.open_file(expandvars(
@@ -233,22 +239,24 @@ def test_generate_ionization_electrons():
                       'r')
     ptab = tf.root.MC.MCTracks
     wval = 22.4e2
-    
+
+    hpxe = HPXeEL()
+
     op = generate_ionization_electrons(
-        ptab, 0, 2.6e6, wval, 0)
-    
+        ptab, 0, 2.6 * units.MeV, hpxe)
+
     E, nrow, b = op
-    
+
     assert len(op) == 3
     assert nrow == -1
     assert b
-    
+
     current_e = 0
     e_ih      = 0
     e_in_hit  = 0
-    
+
     for row in ptab.iterrows():
-        expected_es = int(round(row['hit_energy'] * 10**6 / wval))
+        expected_es = int(round(row['hit_energy'] * 10**6 / hpxe.Wi))
         for e in E[e_ih:]:
             if np.allclose(row['hit_position'], e):
                 e_in_hit  += 1
@@ -258,5 +266,47 @@ def test_generate_ionization_electrons():
                 e_in_hit = 0
                 e_ih = current_e
                 break
-      
-    
+
+def test_box_lengths(b=None):
+    if b == None: b = Box(x_min = -335 * units.mm,
+                          x_max =  235 * units.mm,
+                          y_min =  -35 * units.mm,
+                          y_max =    0 * units.mm,
+                          z_min =    0 * units.mm,
+                          z_max =   53 * units.mm)
+    assert b.length_x() == b.x_max - b.x_min
+    assert b.length_y() == b.y_max - b.y_min
+    assert b.length_z() == b.z_max - b.z_min
+
+def test_box_volume(b=None):
+    if b == None: b = Box()
+    assert b.volume() == b.length_x() * b.length_y() * b.length_z()
+
+def test_tracking_plane_box():
+    b = TrackingPlaneBox()
+    test_box_lengths(b)
+    test_box_volume(b)
+    assert b.in_sipm_plane(-235 * units.mm, -235 * units.mm)
+    assert b.in_sipm_plane( 235 * units.mm,  235 * units.mm)
+    assert b.in_sipm_plane( 235 * units.mm, -235 * units.mm)
+    assert b.in_sipm_plane(-235 * units.mm,  235 * units.mm)
+    assert b.in_sipm_plane(   1 * units.mm,    0 * units.mm)
+
+
+    assert not b.in_sipm_plane(-235.1 * units.mm,    0 * units.mm)
+    assert not b.in_sipm_plane( 236   * units.mm,    0 * units.mm)
+    assert not b.in_sipm_plane(   0   * units.mm,  236 * units.mm)
+    assert not b.in_sipm_plane(   0   * units.mm, -236 * units.mm)
+
+def test_tracking_plane_response_box():
+    assert False
+
+def test_HPXeEL():
+    D = HPXeEL()
+    energy = np.array([2 * units.keV, 3 * units.keV], dtype=np.float32)
+    assert np.isclose(D.YP, 140 * D.EP / units.kilovolt*units.cm*units.bar - 116)
+    assert np.isclose(D.Ng, D.YP * D.d / units.cm * D.P / units.bar)
+
+    assert (D.scintillation_photons(energy) == energy * D.rf / D.Ws).all()
+    assert (D.ionization_electrons (energy) == energy * D.rf / D.Wi).all()
+    assert (D.el_photons           (energy) == energy * D.Ng / D.rf).all()
