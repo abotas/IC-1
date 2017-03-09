a4e84a71ab8cd924ec0aa26ccb1ec4e5ff630bb8
Alejandro Botas <alej@ip-192-168-28-155.private.net>
2017-03-09 16:00:18 +0100

    new bin_EL function

--- invisible_cities/core/detector_response_functions.py
+++ invisible_cities/core/detector_response_functions.py
@@ -17,25 +17,28 @@ class HPXeEL:
     dV   = drift velocity
     P    = pressure
     d    = EL grid gap
-    t    = dustance from the EL anode to the tracking plane
+    t    = distance from the EL anode to the tracking plane
     L    = drift length
     Ws   = energy to produce a scintillation photon
     Wi   = energy to produce a ionization electron
-    fano = sigma = sqrt(fano * hit energy / Wi)
+    ie_fano = sigma = sqrt(ie_fano * hit energy / Wi)
+    g_fano = sigma = sqrt(g_fano * expected photons)
     rf   = electron reduction factor. This is a number that multiplies
            the number of electrons and divides the number of photons, so
            that the product of both is the same (but the number of
            generated electrons is smaller)
     """
-    def __init__(self,EP      =   3.5  * units.kilovolt/ (units.cm * units.bar),
+    def __init__(self,EP      =   3.5  * units.kilovolt / (units.cm * units.bar),
                       dV      =   1.0  * units.mm/units.mus,
                       P       =  15    * units.bar,
                       d       =   5    * units.mm,
                       t       =   5    * units.mm,
+                      t_el    =   2    * units.mus,
                       L       = 530    * units.mm,
                       Ws      =  24    * units.eV,
                       Wi      =  16    * units.eV,
-                      ie_fano =   0.15,
+                      ie_fano =   0.15           ,
+                      g_fano  =   0.1            ,
                       diff_xy =  10    * units.mm/np.sqrt(1 * units.m),
                       diff_z  =   3    * units.mm/np.sqrt(1 * units.m),
                       rf      =   1):
@@ -46,6 +49,8 @@ class HPXeEL:
         self.dV      = dV
         self.P       = P
         self.d       = d
+        self.t       = t
+        self.t_el    = t_el
         self.L       = L
         self.YP      = 140 * ep - 116  # in photons per electron bar^-1 cm^-1
         self.Ng      = self.YP * self.d/units.cm * self.P/units.bar #photons/e
@@ -53,6 +58,7 @@ class HPXeEL:
         self.Wi      = Wi
         self.rf      = rf
         self.ie_fano = ie_fano
+        self. g_fano =  g_fano
 
     def scintillation_photons(self, E):
         return E / self.Ws
@@ -156,84 +162,56 @@ def SiPM_response(e, xpos, ypos, xydim, z_bound, gain):
            -  1.0 / np.sqrt(DX2 + DY2 + z_bound[0]**2)),
            dtype=np.float32)
 
-def bin_EL(E, xpos, ypos, zpos, xydim, zdim, zpitch,
-             el_traverse_time, el_width, el_sipm_d, t_gain, gain_nf):
+def bin_EL(E, hpxe, rb):
     """
     arguments:
-    TS, time of electron arrival to start of EL plane in units
-    of time bins
-    E, ndarray of electrons for an event
+    E   , a np.array of all the diffused electrons from a single hit
+    rb, a TrackingPlaneResponseBox
+    hpxe, a HPXeEL
 
     returns:
-    ev_maps, a 20x20x60 np.array containing the SiPM maps for
-    an event.
-
-    -computes fraction of photons registered in each time bin
-    -calls SiPM_response twice, once for each time bin
+    the effect of the EL
+    -G  the photons produced by each electron in each z-time bin
+    -IB the integration boundaries (zdist from el from which photons are being
+     cast for each electron for in each time bin)
     """
-
-    # SiPM maps for an event
-    ev_maps = np.zeros((xydim, xydim, zdim), dtype=np.float32)
-
-    # num maps that will receive photons form each electron
-    num_bins = max(2, int(el_traverse_time / zpitch) + 1)
-
+    # Fraction of Gain and Integration Boundaries for each e for each z in z_pos
+    FG  = np.zeros((len(E), len(rb.z_pos)),     dtype=np.float32)
+    IB  = np.zeros((len(E), len(rb.z_pos) , 2), dtype=np.float32) + hpxe.d + hpxe.t
     # z in units of time bin
-    TS = (E[:, 2] - zpos[0]) / zpitch
+    TS  = (E[:, 2] - rb.z_pos[0]) / rb.z_pitch
+    fTS = np.array(np.floor(TS), dtype=np.int16) # int16 if zdim > 127!
 
-    # Fraction of photons detected in each time bin
-    FG   = np.empty((len(TS), num_bins), dtype=np.float32)
-    f_TS = np.array(np.floor(TS), dtype=np.int8) # int16 if zdim > 127!
+    for i, (e, f, ts) in enumerate(zip(E, fTS, TS)):
+        for j, z in enumerate(rb.z_pos):
+            # electron enters EL between z and z + z_pitch
+            if j == f:
+                # Calculate gain in first responsive slice
+                fg = min((j + 1 - ts) * rb.z_pitch / hpxe.t_el, 1)
+                # Compute distance integration boundaries for time bin
+                s_d = hpxe.d + hpxe.t
+                ib  = [s_d, s_d - fg * hpxe.d]
+                FG[i, j] = fg
+                IB[i, j] = np.array(ib, dtype=np.float32)
+            # electron is still crossing EL
+            elif j > f and z < e[2] + hpxe.t_el and f > 0:
+                # electron crossing EL over entire time bin
+                if z + rb.z_pitch  <= e[2] + hpxe.t_el:
+                    fg = rb.z_pitch / hpxe.t_el
+                    s_d = hpxe.d + hpxe.t - (z - e[2]) / hpxe.t_el * hpxe.d
+                    ib  = [s_d, s_d - fg * hpxe.d]
+                    FG[i, j] = fg
+                    IB[i, j] = np.array(ib, dtype=np.float32)
+                # electron finishes crossing EL during time bin
+                else:
+                    fg  = (e[2] + hpxe.t_el - z) / hpxe.t_el
+                    s_d = hpxe.d + hpxe.t - (z - e[2]) / hpxe.t_el * hpxe.d
+                    ib  = [s_d, hpxe.t]
+                    FG[i, j] = fg
+                    IB[i, j] = np.array(ib, dtype=np.float32)
+                    #if not np.allclose(hpxe.t, s_d - fg * hpxe.t_el):
+                    #    raise ValueError('final integ boundary != hpxe.t')
 
-    # Don't divide by 0
-    if el_traverse_time == 0:
-        FG[:, 0] = 1 # All photons sent to first time bin
-        FG[:, 1] = 0
-    else:
-        # Compute the fraction of gain in each relevant map
-        FG[:,     0] = np.minimum(
-                       (1 + f_TS - TS) * zpitch, el_traverse_time) \
-                       / float(el_traverse_time)
-        FG[:, 1: -1] = zpitch / float(el_traverse_time) # all the same
-        FG[:,    -1] = 1 \
-                       - FG[:, 0] \
-                       - (num_bins - 2) * zpitch / float(el_traverse_time)
-
-    #** Note fraction of gain in each map is equivalent to fraction of
-    #   of EL traversed by electron during time bin associated with map
-    # compute z DISTANCE integration boundaries for each time bin
-    z_integ_bound = np.ones((len(TS), num_bins + 1), dtype=np.float32)
-    z_integ_bound[:, 0] = el_width + el_sipm_d
-
-    for i in range(num_bins):
-        z_integ_bound[:, i + 1] = z_integ_bound[:, i] - FG[:, i] * el_width
-
-    if not np.allclose(z_integ_bound[:, -1], el_sipm_d):
-        raise ValueError('final z_integ_bound not el_sipm_d')
-
-    # compute gain for electron
-    if gain_nf != 0:
-        G = np.random.normal(t_gain,
-                      scale=np.sqrt(t_gain * gain_nf),
-                      size=(len(E),))
-
-    else: G = np.ones((len(E),), dtype=np.float32) * t_gain
-
-    # for each electron
-    for f_ts, fg_e, e, zbs, g in zip(f_TS, FG, E, z_integ_bound, G):
-
-        # for each time bin
-        for b, fg in enumerate(fg_e):
-
-            # some electrons have bins before z window start
-            if f_ts + b >= 0 and not np.isclose(zbs[b], zbs[b + 1]):
-                try:
-                    # get electron's contribution to this map
-                    ev_maps[:, :, f_ts + b] = SiPM_response(e, xpos, ypos,
-                       xydim, zbs[[b, b + 1]], fg * g)
-
-                # Outside z-window
-                except IndexError:
-                    if f_ts > zdim: raise
-
-    return ev_maps
+    F  = FG * hpxe.Ng
+    F += np.random.normal(scale=np.sqrt(F * hpxe.g_fano))
+    return F, IB
