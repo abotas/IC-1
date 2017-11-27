ca49c5a746650558255d0d5266c26f16601ee96b
Alejandro Botas <alejbotas@gmail.com>
2017-11-27 17:38:03 +0100

    Add test for SensorResponses class

--- invisible_cities/evm/new_pmaps_test.py
+++ invisible_cities/evm/new_pmaps_test.py
@@ -1,14 +1,23 @@
+
+import numpy as np
+
+from pytest import approx
+from pytest import raises
+
+from hypothesis             import given
 from hypothesis.strategies  import integers
 from hypothesis.strategies  import floats
 from hypothesis.strategies  import sampled_from
 from hypothesis.strategies  import composite
 from hypothesis.extra.numpy import arrays
 
-from . new_pmaps import  PMTResponses
-from . new_pmaps import SiPMResponses
-from . new_pmaps import S1
-from . new_pmaps import S2
-from . new_pmaps import PMap
+from .. core.core_functions import weighted_mean_and_std
+
+from  . new_pmaps import  PMTResponses
+from  . new_pmaps import SiPMResponses
+from  . new_pmaps import S1
+from  . new_pmaps import S2
+from  . new_pmaps import PMap
 
 
 wf_min =   0
@@ -49,3 +58,55 @@ def pmaps(draw):
     s2s  = tuple(draw(peaks(S2)) for i in range(n_s2))
     args = s1s, s2s
     return args, PMap(*args)
+
+
+@given(sensor_responses())
+def test_SensorResponses_all_waveforms(srs):
+    (_, all_waveforms), sr = srs
+    assert (all_waveforms == sr.all_waveforms).all()
+
+
+@given(sensor_responses())
+def test_SensorResponses_ids(srs):
+    (ids, _), sr = srs
+    assert (ids == sr.ids).all()
+
+
+@given(sensor_responses())
+def test_SensorResponses_waveform(srs):
+    (ids, all_waveforms), sr = srs
+    for sensor_id, waveform in zip(ids, all_waveforms):
+        assert (waveform == sr.waveform(sensor_id)).all()
+
+
+@given(sensor_responses())
+def test_SensorResponses_time_slice(srs):
+    (_, all_waveforms), sr = srs
+    for i, time_slice in enumerate(all_waveforms.T):
+        assert (time_slice == sr.time_slice(i)).all()
+
+
+@given(sensor_responses())
+def test_SensorResponses_sum_over_times(srs):
+    (_, all_waveforms), sr = srs
+    assert np.sum(all_waveforms, axis=1) == approx(sr.sum_over_times)
+
+
+@given(sensor_responses())
+def test_SensorResponses_sum_over_sensors(srs):
+    (_, all_waveforms), sr = srs
+    assert np.sum(all_waveforms, axis=0) == approx(sr.sum_over_sensors)
+
+
+@given(sampled_from((PMTResponses, SiPMResponses)), integers(1, 10))
+def test_SensorResponses_raises_exception_when_shapes_dont_match(SR, a):
+    with raises(ValueError):
+        sr = SR(np.empty(a),
+                np.empty((a + 1, 1)))
+
+
+def assert_SensorResponses_equality(sr0, sr1):
+    # This is sufficient to assert equality since all of SensorResponses other
+    # properties depend solely on .all_waveforms, and all of those properties
+    # are tested.
+    assert (sr0.all_waveforms == sr1.all_waveforms).all()
