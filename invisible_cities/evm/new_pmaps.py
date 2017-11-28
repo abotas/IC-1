0f52df59df9c954377917c04b34b0a9af0d60156
Alejandro Botas <alejbotas@gmail.com>
2017-11-28 13:11:58 +0100

    Implement SensorResponses

--- invisible_cities/evm/new_pmaps.py
+++ invisible_cities/evm/new_pmaps.py
@@ -51,20 +51,62 @@ class S1(_Peak): pass
 class S2(_Peak): pass
 
 
-class SensorResponses:
+class _SensorResponses:
+    _type = "Undefined"
+
+    def __init__(self, ids, wfs):
+        if len(ids) != len(wfs):
+            msg  =  "Shapes do not match\n"
+            msg += f"ids has length {len(ids)}\n"
+            msg += f"wfs has length {len(wfs)}\n"
+            raise ValueError(msg)
+
+        self._ids              = np.asarray(ids)
+        self._all_waveforms    = np.asarray(wfs)
+        self._wfs_dict         = dict(zip(self._ids, self._all_waveforms))
+        self._sum_over_times   = np.sum(self._all_waveforms, axis=1)
+        self._sum_over_sensors = np.sum(self._all_waveforms, axis=0)
 
     @property
-    def all_waveforms(self): pass
+    def all_waveforms(self):
+        return self._all_waveforms
 
-    def waveform(self, sensor_id): pass
+    def waveform(self, sensor_id):
+        return self._wfs_dict[sensor_id]
 
-    def time_slice(self, slice_number): pass
-
-    @property
-    def ids(self): pass
-
-    @property
-    def sum_over_times(self): pass
+    def time_slice(self, slice_number):
+        return self._all_waveforms[:, slice_number]
 
     @property
-    def sum_over_sensors(self): pass
+    def ids(self):
+        return self._ids
+
+    @property
+    def sum_over_times(self):
+        return self._sum_over_times
+
+    @property
+    def sum_over_sensors(self):
+        return self._sum_over_sensors
+
+    def __str__(self):
+        n_sensors = len(self.ids)
+        s  =  "------------------------\n"
+        s += f"{self._type} instance\n"
+        s +=  "------------------------\n"
+        s += f"Number of sensors: {n_sensors}\n"
+        for ID, wf in self._wfs_dict.items():
+            s += f"| ID: {ID}\n"
+            s += f"| WF: {wf}\n"
+            s +=  "| \n"
+        return s + "\n"
+
+    __repr__ = __str__
+
+
+class PMTResponses(_SensorResponses):
+    _type = "PMTResponses"
+
+
+class SiPMResponses(_SensorResponses):
+    _type = "SiPMResponses"
